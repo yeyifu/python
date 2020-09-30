f3b8c483008f543a71e483cb0cddfa8c988d69e3
Administrator <admin@example.com>
2020-09-30 10:14:24 +0800

    学习案例笔记-20200930

---
+++ day2/sys_mod.py
@@ -0,0 +1,17 @@
+"""
+import sys
+print(sys.path)#打印环境变量
+print(sys.argv)#打印当前路径
+"""
+
+"""
+import os
+# os.system("dir")#只执行命令，不保留结果
+
+# cmd_res = os.popen("dir").read() #查看目录
+# print(cmd_res)
+
+# os.mkdir("new_dir") #新建文件夹
+"""
+
+import login
